a7d45fda91b6e25e01f139c54eba409c6f297249
Carlos <augusto.rj.sp@gmail.com>
2020-08-09 16:26:49 -0300

    commit

---
+++ src/main/java/papai/Pai.java
@@ -0,0 +1,9 @@
+package papai;
+
+public class Pai {
+
+    public static void main(String[] args){
+
+        System.out.println("FELIZ DIA DOS PAIS PARA TODOS OS PAIS QUE ESTÃO PASSANDO POR 2020...!!!");
+    }
+}
